bf8849ea8550173ef214f3878fb230b23da5977c
ruanvsrateira <vruan85@gmail.com>
2023-10-11 16:46:11 -0300

    feat: about command order by

--- scripts.sql
+++ scripts.sql
@@ -6,3 +6,6 @@ SELECT set_time, set_by  FROM sys_config;
 
 -- Comando para secionar todos as colunas da tabela 
 SELECT * FROM sys_config;
+
+-- Comando para ordenar pela coluna
+SELECT sc.set_time FROM sys_config sc ORDER BY sc.set_time;
