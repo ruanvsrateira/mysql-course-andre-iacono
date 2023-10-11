600b58f1fab2ab3dd8c8d2e13339edc7463d891a
ruanvsrateira <vruan85@gmail.com>
2023-10-11 16:50:57 -0300

    feat: about command where and combinating with limit

--- scripts.sql
+++ scripts.sql
@@ -9,3 +9,10 @@ SELECT * FROM sys_config;
 
 -- Comando para ordenar pela coluna
 SELECT sc.set_time FROM sys_config sc ORDER BY sc.set_time;
+
+-- Comando para fazer comparação do valor e assim realizar filtro
+SELECT * from sys_config sc WHERE sc.variable = "diagnostics.allow_i_s_tables";
+SELECT * from sys_config sc WHERE sc.value < 100;
+
+-- Podemos também estar limitando quantas rows ele vai retornar
+SELECT * from sys_config sc WHERE sc.value < 100;
